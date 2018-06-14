2b6e73b582c12630a071c9273a151cda2280aec3
Tracy Lum <tracy@flatironschool.com>
2018-06-14 12:50:38 +0000

    automatically backed up by learn

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -20,15 +20,15 @@ require "spec_helper"
      end
    end
    describe "#average_student_gpa" do
-     it 'finds the average gpa' do
+     xit 'finds the average gpa' do
      end
    end
    describe "#total_tardies_for_all_students" do
-     it 'finds the total amount of tardies for all students' do
+     xit 'finds the total amount of tardies for all students' do
      end
    end
    describe "#average_gpa_for_9th_grade" do
-     it 'find the average gpa for 9th grade' do
+     xit 'find the average gpa for 9th grade' do
      end
    end
 
